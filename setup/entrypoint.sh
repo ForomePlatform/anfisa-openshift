c44ce794d69219e072611f93545164ba7ae1b6c8
evgeniyabrosin <91061789+evgeniyabrosin@users.noreply.github.com>
2021-11-20 02:08:43 +0300

    Update entrypoint.sh

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -8,9 +8,9 @@ if [ ! -f /anfisa/a-setup/export/SEQaBOO_output_template_20190317.xlsx ]  ; then
   popd
 fi
 
-if [ ! -z ${ANFISA_COORD_HOST+z} ] && [ ! -z ${ANFISA_ROUTER_HOST+z} ] && [ ! -z ${ANFISA_MONGO_HOST+z} ] ;
-then
-    sed "s#anfisa6-coordinator#${ANFISA_COORD_HOST}#g" /anfisa/anfisa.json | sed "s#anfisa6-router#${ANFISA_ROUTER_HOST}#g" | sed "s#anfisa6-mongo#${ANFISA_MONGO_HOST}#g" -  > /anfisa/anfisa.json
-fi
+#if [ ! -z ${ANFISA_COORD_HOST+z} ] && [ ! -z ${ANFISA_ROUTER_HOST+z} ] && [ ! -z ${ANFISA_MONGO_HOST+z} ] ;
+#then
+#    sed "s#anfisa6-coordinator#${ANFISA_COORD_HOST}#g" /anfisa/anfisa.json | sed "s#anfisa6-router#${ANFISA_ROUTER_HOST}#g" | sed "s#anfisa6-mongo#${ANFISA_MONGO_HOST}#g" -  > /anfisa/anfisa.json
+#fi
 
 exec "$@"

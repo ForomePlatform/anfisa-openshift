cba78abeaa2953a77f526a1791b9f59997a16602
evgeniyabrosin <91061789+evgeniyabrosin@users.noreply.github.com>
2021-11-20 01:17:04 +0300

    Update entrypoint.sh

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -10,7 +10,7 @@ fi
 
 if [ ! -z ${ANFISA_COORD_HOST+z} ] && [ ! -z ${ANFISA_ROUTER_HOST+z} ] && [ ! -z ${ANFISA_MONGO_HOST+z} ] ;
 then
-    sed "s#anfisa-druid-coordinator#${ANFISA_COORD_HOST}#g" /anfisa/anfisa.json | sed "s#router#${ANFISA_ROUTER_HOST}#g" | sed "s#anfisa-mongodb#${ANFISA_MONGO_HOST}#g" -  > /anfisa/anfisa.json
+    cat /anfisa/anfisa/setup/anfisa.json.docker | sed "s#anfisa-druid-coordinator:8081#${ANFISA_COORD_HOST}#g" | sed "s#router:8888#${ANFISA_ROUTER_HOST}#g" | sed "s#anfisa-mongodb#${ANFISA_MONGO_HOST}#g" -  > /anfisa/anfisa.json
 fi
 
 exec "$@"

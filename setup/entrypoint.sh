63d706e61d970e99d82b8100f3c8534f6342c82a
evgeniyabrosin <91061789+evgeniyabrosin@users.noreply.github.com>
2021-11-24 14:36:12 +0300

    Update entrypoint.sh

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-for d in /anfisa/a-setup/{data,logs,vault,export/work,ui} ; do
-    [ ! -d $d ] && mkdir -p $d
-done
+
+#for d in /anfisa/a-setup/{data,logs,vault,export/work,ui} ; do
+#    [ ! -d $d ] && mkdir -p $d
+#done
+
 if [ ! -f /anfisa/a-setup/export/SEQaBOO_output_template_20190317.xlsx ]  ; then
   pushd /anfisa/a-setup/export
   curl -L -O https://www.dropbox.com/s/4dvunn3dusqc636/SEQaBOO_output_template_20190317.xlsx

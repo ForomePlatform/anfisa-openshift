b37ccdcbf681fcb5e150bc1547d1070687552cd8
evgeniyabrosin <91061789+evgeniyabrosin@users.noreply.github.com>
2021-11-25 13:08:20 +0300

    Update entrypoint.sh

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-
-#export COUNT_FILES=$(ls -A /anfisa/a-setup/ | wc -l)
-#if [ ${COUNT_FILES} -le 2 ] ; then
-#    mkdir -p /anfisa/a-setup/{data,logs,vault,export/work,ui}
-#    chmod -R 00755 /anfisa/a-setup/
-#fi
-
-if [ ! -f /anfisa/a-setup/export/SEQaBOO_output_template_20190317.xlsx ]  ; then
-  pushd /anfisa/a-setup/export
-  curl -L -O https://www.dropbox.com/s/4dvunn3dusqc636/SEQaBOO_output_template_20190317.xlsx
-  popd
+export COUNT_FILES=$(ls -A /anfisa/a-setup/ | wc -l)
+if [ ${COUNT_FILES} -le 2 ] ; then
+    mkdir -p /anfisa/a-setup/{data,logs,vault,export/work,ui}
 fi
-
-#if [ ! -z ${ANFISA_COORD_HOST+z} ] && [ ! -z ${ANFISA_ROUTER_HOST+z} ] && [ ! -z ${ANFISA_MONGO_HOST+z} ] ;
-#then
-#    sed "s#anfisa6-coordinator#${ANFISA_COORD_HOST}#g" /anfisa/anfisa.json | sed "s#anfisa6-router#${ANFISA_ROUTER_HOST}#g" | sed "s#anfisa6-mongo#${ANFISA_MONGO_HOST}#g" -  > /anfisa/anfisa.json
-#fi
 
 exec "$@"
